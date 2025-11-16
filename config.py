7b098ed81ee470f44e88fa8a2c1ca8dd454d94d9
Yerzat-ai <61986207+Yerzat-ai@users.noreply.github.com>
2025-11-16 20:12:20 +0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,19 @@
+BOT_TOKEN = "8328803733:AAGaiU5KqpURqiYhBklA92r_onsVnz9kC3Q"
+
+FIREBASE_CONFIG = {
+    "type": "service_account",
+    "project_id": "",
+    "private_key_id": "",
+    "private_key": "",
+    "client_email": "",
+    "client_id": "",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": ""
+}
+
+PEPE_PRICE = 0.00001
+REWARD_PERCENT = 0.20   # 20%
+INVITE_NOTCOIN_REWARD = 50
+MIN_WITHDRAW_TON = 10
